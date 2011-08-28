3210c6a55614aea522c16d1a914f0535c6ceabf9
dfreerksen <dfreerksen@gmail.com>
2011-08-28 12:33:26 -0700

    Update to database file

--- acl.sql
+++ acl.sql
@@ -1,28 +1,31 @@
-# Sequel Pro dump
-# Version 2492
-# http://code.google.com/p/sequel-pro
-#
-# Host: localhost (MySQL 5.1.44)
-# Database: acl
-# Generation Time: 2011-05-06 14:42:38 -0700
 # ************************************************************
+# Sequel Pro SQL dump
+# Version 3348
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: localhost (MySQL 5.5.9)
+# Database: auth_acl
+# Generation Time: 2011-08-28 19:32:05 +0000
+# ************************************************************
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-# Dump of table a_permissions
+# Dump of table permissions
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `a_permissions`;
+DROP TABLE IF EXISTS `permissions`;
 
-CREATE TABLE `a_permissions` (
+CREATE TABLE `permissions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `key` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -30,11 +33,12 @@ CREATE TABLE `a_permissions` (
   `group` varchar(255) NOT NULL DEFAULT 'Unknown',
   `order` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
-LOCK TABLES `a_permissions` WRITE;
-/*!40000 ALTER TABLE `a_permissions` DISABLE KEYS */;
-INSERT INTO `a_permissions` (`id`,`key`,`name`,`description`,`group`,`order`)
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+
+INSERT INTO `permissions` (`id`, `key`, `name`, `description`, `group`, `order`)
 VALUES
 	(1,'products-edit','Edit Products','Edit product information such as SKU, brand, description, specs, etc','Products',2),
 	(2,'products-delete','Delete Products','Remove products from the site.','Products',3),
@@ -45,25 +49,30 @@ VALUES
 	(7,'category-delete','Delete Categories','Remove categories.','Category',3),
 	(8,'something-else','Something','Ability to do something','Unknown',1);
 
-/*!40000 ALTER TABLE `a_permissions` ENABLE KEYS */;
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
-# Dump of table a_role_permissions
+# Dump of table role_permissions
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `a_role_permissions`;
+DROP TABLE IF EXISTS `role_permissions`;
 
-CREATE TABLE `a_role_permissions` (
+CREATE TABLE `role_permissions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `role_id` int(11) NOT NULL,
-  `permission_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+  `role_id` int(11) unsigned NOT NULL,
+  `permission_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  KEY `permission_id` (`permission_id`),
+  CONSTRAINT `role_permissions_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `role_permissions_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
-LOCK TABLES `a_role_permissions` WRITE;
-/*!40000 ALTER TABLE `a_role_permissions` DISABLE KEYS */;
-INSERT INTO `a_role_permissions` (`id`,`role_id`,`permission_id`)
+LOCK TABLES `role_permissions` WRITE;
+/*!40000 ALTER TABLE `role_permissions` DISABLE KEYS */;
+
+INSERT INTO `role_permissions` (`id`, `role_id`, `permission_id`)
 VALUES
 	(1,1,1),
 	(2,1,2),
@@ -77,63 +86,70 @@ VALUES
 	(10,2,2),
 	(11,2,8);
 
-/*!40000 ALTER TABLE `a_role_permissions` ENABLE KEYS */;
+/*!40000 ALTER TABLE `role_permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
-# Dump of table a_roles
+# Dump of table roles
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `a_roles`;
+DROP TABLE IF EXISTS `roles`;
 
-CREATE TABLE `a_roles` (
+CREATE TABLE `roles` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET latin1 NOT NULL,
   `description` text CHARACTER SET latin1,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-LOCK TABLES `a_roles` WRITE;
-/*!40000 ALTER TABLE `a_roles` DISABLE KEYS */;
-INSERT INTO `a_roles` (`id`,`name`,`description`)
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+
+INSERT INTO `roles` (`id`, `name`, `description`)
 VALUES
 	(1,'Admin','Administrator. Has access to everything.'),
 	(2,'Another Account','Another account where permissions can be assigned to.');
 
-/*!40000 ALTER TABLE `a_roles` ENABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
-# Dump of table a_users
+# Dump of table users
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `a_users`;
+DROP TABLE IF EXISTS `users`;
 
-CREATE TABLE `a_users` (
+CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `role_id` int(11) NOT NULL,
+  `role_id` int(11) unsigned NOT NULL,
+  `email` varchar(255) NOT NULL,
   `username` varchar(255) CHARACTER SET latin1 NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+  `password` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `date_created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
-LOCK TABLES `a_users` WRITE;
-/*!40000 ALTER TABLE `a_users` DISABLE KEYS */;
-INSERT INTO `a_users` (`id`,`role_id`,`username`)
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `role_id`, `email`, `username`, `password`, `name`, `last_login`, `date_created`, `active`)
 VALUES
-	(1,1,'Administrator'),
-	(2,2,'User');
+	(15,1,'admin@example.com','admin','cb3ee1267897be7d1f6774550068c3051704cc8f','Admninistrator',NULL,'2011-08-18 06:14:54',1),
+	(16,2,'user@example.com','user','cb3ee1267897be7d1f6774550068c3051704cc8f','User',NULL,'2011-08-18 06:15:37',1);
 
-/*!40000 ALTER TABLE `a_users` ENABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
 
 
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
